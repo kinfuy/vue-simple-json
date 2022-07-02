23c9ff2b94f6840f0428c0353aec09446ca6ebdf
alqmc <Yonghu520@outlook.com>
2022-07-02 18:39:20 +0800

    fix: docs base

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuejsx from '@vitejs/plugin-vue-jsx';
 export default defineConfig({
+  base: 'vue-simple-json',
   build: {
     outDir: '../docs',
   },
