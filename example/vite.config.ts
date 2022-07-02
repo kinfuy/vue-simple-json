8c3e14a4152489cb6b28b399679e26a5c89ddde6
alqmc <Yonghu520@outlook.com>
2022-07-02 18:24:43 +0800

    feat: docs

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuejsx from '@vitejs/plugin-vue-jsx';
 export default defineConfig({
+  build: {
+    outDir: '../docs',
+  },
   plugins: [vue(), vuejsx()],
 });
